c46722c197037326213c91695f98ea7f2095c9f2
Akshat Chugh <akshatchugh3@gmail.com>
2018-10-14 01:59:21 +0530

    Small changes in create-tables.py

--- create_tables.py
+++ create_tables.py
@@ -17,15 +17,16 @@ if __name__ == '__main__':
                  bowling_skill TEXT, country_name TEXT,\
                   PRIMARY KEY (player_id)) ;")
 
-    cur.execute("CREATE TABLE PLAYER_MATCH (playermatch_key BIGINT, match_id INT, player_id INT, batting_hand TEXT, \
+    cur.execute("CREATE TABLE PLAYER_MATCH (playermatch_key INT, match_id INT, player_id INT, batting_hand TEXT, \
                 bowling_skill TEXT, role_desc TEXT, team_id INT, PRIMARY KEY (playermatch_key),\
                 FOREIGN KEY(match_id) REFERENCES MATCH(match_id), \
                 FOREIGN KEY(player_id) REFERENCES PLAYER(player_id)) ;")
 
-    cur.execute("CREATE TABLE BALL_BY_BALL (match_id INT UNIQUE, innings_no INT UNIQUE, over_id INT UNIQUE, ball_id INT, \
+    cur.execute("CREATE TABLE BALL_BY_BALL (match_id INT, innings_no INT, over_id INT, ball_id INT, \
                 striker_batting_position INT, runs_scored INT, \
                 extra_runs INT, out_type TEXT, striker INT, non_striker INT, bowler INT, \
-                PRIMARY KEY (ball_id), \
+                PRIMARY KEY (match_id, innings_no, over_id, ball_id), \
+                FOREIGN KEY (match_id) REFERENCES  MATCH (match_id), \
                 FOREIGN KEY (striker) REFERENCES PLAYER(player_id), \
                 FOREIGN KEY (non_striker) REFERENCES PLAYER(player_id), \
                 FOREIGN KEY (bowler) REFERENCES PLAYER(player_id) \
