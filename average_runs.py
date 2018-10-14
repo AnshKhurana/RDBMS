16b1b0fc6b96bdbc0ea8792ea0d1e561a9f9e701
AnshKhurana <anshkhurana1507@gmail.com>
2018-10-14 17:48:15 +0530

    Save

--- average_runs.py
+++ average_runs.py
@@ -4,7 +4,7 @@ if __name__ == '__main__':
     ipl = sqlite3.connect('ipl.db')
     c = ipl.cursor()
 
-    c.execute("SELECT venue_name, AVG(SUM(runs_scored)), COUNT(venue_name) FROM MATCH, BALL_BY_BALL \
+    c.execute("SELECT venue_name, SUM(runs_scored), COUNT(venue_name) FROM MATCH, BALL_BY_BALL \
               WHERE MATCH.match_id = BALL_BY_BALL.match_id \
                 GROUP BY venue_name ORDER BY 2 DESC ;")
     for row in c:
