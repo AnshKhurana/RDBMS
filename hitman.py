9d6f881e2c884f6e30c40a915f6c51ff4a4ecd26
Akshat Chugh <akshatchugh3@gmail.com>
2018-10-15 20:50:56 +0530

    Task 6a had a misconception. Updated now

--- hitman.py
+++ hitman.py
@@ -3,11 +3,9 @@ import sqlite3
 if __name__ == '__main__':
     ipl = sqlite3.connect('ipl.db')
     cur = ipl.cursor()
-    cur.execute("SELECT player_id, player_name, B.sixes , C.balls, (B.sixes*1.0/C.balls) ans FROM PLAYER \
-                JOIN (SELECT striker, COUNT(runs_scored) sixes \
-                FROM BALL_BY_BALL WHERE runs_scored = 6 GROUP BY striker HAVING sixes > 0) B \
-                ON PLAYER.player_id = B.striker \
-                JOIN (SELECT striker, COUNT(striker) balls FROM BALL_BY_BALL GROUP BY striker) C\
+    cur.execute("SELECT player_id, player_name, C.sixes , C.balls, (C.sixes*1.0/C.balls) ans FROM PLAYER \
+                JOIN (SELECT striker, COUNT(striker) balls, (SELECT COUNT(runs_scored) FROM BALL_BY_BALL b2 \
+                WHERE b2.striker=b1.striker AND runs_scored=6) sixes FROM BALL_BY_BALL b1 GROUP BY striker) C\
                 ON PLAYER.player_id = C.striker\
                 GROUP BY player_id \
                 ORDER BY ans DESC;")
